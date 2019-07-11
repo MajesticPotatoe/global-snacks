005adf6df797e88588349780838bb62dad7c22e6
Andrew Henry <ahenry@fcp.biz>
2019-07-11 13:38:27 -0400

    feat: create snackbar component

---
+++ src/components/AppSnackbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-snackbar
+    v-model="snackbar.show"
+    :color="snackbar.color"
+    :timeout="snackbar.timeout"
+    auto-height
+    right
+    top
+  >
+    {{ snackbar.text }}
+    <v-btn
+      fab
+      small
+      text
+      @click="snackbar.show = false"
+    >
+      <v-icon>mdi-close</v-icon>
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+  import { sync } from 'vuex-pathify'
+
+  export default {
+    data: () => ({}),
+    computed: {
+      ...sync('appSnackbar/*')
+    }
+  }
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-layout>
+    Home
+  </v-layout>
+</template>
+
+<script>
+export default {
+  //
+};
+</script>
